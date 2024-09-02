1af8ec74993d3cc9b75d0f5154367d06ec844b9a
Balinder <sultanpal81@gmail.com>
2024-09-02 19:53:04 +0530

    modified Assignment5

--- Assignment5.js
+++ Assignment5.js
@@ -1,2 +1,24 @@
+// Find the length of string.
 // code is written by sultan 
+
 // NOTE : plz ignore spelling. and focus to learn.
+
+// Assignment 6 : uploading file and then display file content.
+
+    document.getElementById("fileinput").addEventListener('change',function(event){
+        const file = event.target.files[0];
+        if(file){
+            const reader = new FileReader();
+            reader.onload = function(e){
+                document.getElementById("fileContent").textContent = e.target.result;
+            };
+            reader.readAsText(file);
+        }
+    });
+
+
+
+    // use this code for html 
+    // <input type="file" id="fileinput" name="ufile">
+    // <div id="fileContent"></div>
+
